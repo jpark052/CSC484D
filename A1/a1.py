89dfd0914b9070354fabc20e5a8a68c6f909a53a
Jae Park <jaepark052@gmail.com>
2023-01-22 22:56:47 -0800

    complete q3 of a1

--- A1/a1.py
+++ A1/a1.py
@@ -12,7 +12,7 @@ def create_sinusoid(frequency=440, dur=1, srate=44100, phase = 0):
     data = amp * np.sin(2*np.pi*frequency *t+phase)
     return data
 
-data = create_sinusoid(frequency=220, dur=3, srate=44100)
+data = create_sinusoid(frequency=440, dur=3, srate=44100)
 # data2 = create_sinusoid(frequency=440, dur=3, srate=44100)
 # sum = np.concatenate((data, data2), axis=None)
 
@@ -27,7 +27,6 @@ midiList=[(45,1),(60,1),(68,2)]
 def midi_to_freq(midiList: list):
     melody = []
     for midiNote in midiList:
-        print("hi")
         if (midiNote[0] < 0 or midiNote[0] > 127):
             print("please enter valid mini note number")
             return
@@ -43,3 +42,27 @@ mel=midi_to_freq(midiList)
 write("Q2.wav", srate, mel.astype(np.int16))
 
 #q3
+# [noteon, time, pitch(frequency)]
+SKINIlist = ["NoteOn          2 220",
+    "NoteOff         3 110",
+    "NoteOn          1 440"]
+
+
+def SKINI_to_sound(skiniList: list[str]):
+    melody = []
+    print(SKINIlist)
+
+    for oneLine in skiniList:
+        print(oneLine)
+        skiniMsg = oneLine.split()
+
+        if (len(skiniMsg) != 0):
+            data = create_sinusoid(frequency=int(skiniMsg[2]), dur=int(skiniMsg[1]))
+            melody = np.concatenate((melody, data), axis=None)
+
+    return melody
+
+mel=SKINI_to_sound(SKINIlist)
+write("Q3.wav", srate, mel.astype(np.int16))
+
+#q4
